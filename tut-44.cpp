b50501e13920041a86f0c6b21c3ef2022ee4a5af
fardin36 <thunder.fardin@gmail.com>
2024-01-17 23:43:57 +0600

    tut-44.cpp updated

--- tut-44.cpp
+++ tut-44.cpp
@@ -1,1 +1,27 @@
 // Virtual Base Class in C++
+
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void say()
+    {
+        cout << "Hello World!!" << endl;
+    }
+};
+
+class B : public virtual A
+{
+};
+class C : public virtual A
+{
+};
+class D : public B, public C
+{
+};
+
+int main()
+{
+    return 0;
+}
